640bb3e971c9f9dbaca186fe5e9309447acf988e
Wojciech Frącz <fraczwojciech@gmail.com>
2025-12-10 18:44:17 +0100

    First working version

--- src/index.js
+++ src/index.js
@@ -1,15 +1,50 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { createConnection } from 'mysql2/promise';
+import PostalMime from "postal-mime";
+import { convert } from "html-to-text";
+
+const parseContent = (text, html) => {
+	let body = text;
+	if (!body && html) {
+		body = convert(html);
+	}
+	if (!body) {
+		return null;
+	}
+	return body.trim();
+};
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+	async email(message, env, ctx) {
+
+		const email = await PostalMime.parse(message.raw, {
+			attachmentEncoding: "base64",
+		});
+
+		console.log({
+			text: email.text,
+			html: email.html
+		});
+
+		let body = parseContent(email.text, email.html);
+
+		const connection = await createConnection({
+			host: 'mws02.mikr.us',
+			user: 'cloudflare_gc',
+			password: env.MYSQL_PASS,
+			database: 'geocaching',
+			port: 50121,
+			disableEval: true
+		});
+
+		try {
+			await connection.execute(
+				'INSERT INTO gcfound_email (subject, content) VALUES(?,?)',
+				[email.subject, body]
+			);
+
+			ctx.waitUntil(connection.end());
+		} catch (e) {
+			console.error(e);
+		}
+	}
 };
